74f81395450692626c843b3648f5c608fe5c1629
mahasiswa229 <161399711+mahasiswa229@users.noreply.github.com>
2024-05-11 13:10:28 +0800

    class stack pt6

--- stack.cpp
+++ stack.cpp
@@ -51,6 +51,15 @@ class Stack
         {
             cout << "List is Empty" << endl;
         }
-        
+        else
+        {
+            Node* current= top;
+            while (current!=NULL)
+            {
+                cout << current ->data << " " << endl;
+                current = current -> next;
+            }
+             cout << endl;
+        }
     }
 };
