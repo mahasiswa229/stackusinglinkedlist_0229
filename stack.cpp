98ecc5d0deccfe835ee5dc95559874f81fa8b37e
mahasiswa229 <161399711+mahasiswa229@users.noreply.github.com>
2024-05-11 13:25:15 +0800

    switch case pt2

--- stack.cpp
+++ stack.cpp
@@ -91,7 +91,16 @@ int main()
             cin >> value;
             stack.push(value);
             break;
-        
+        case 2:
+        if (!stack.isEmpty())
+        {
+            stack.pop();
+        }
+        else
+        {
+            cout << "Stack is Empty.Cannot Pop." << endl;
+        }
+        break;
         default:
             break;
         }
